470c47fcb2eabece2e51b369e505dae6e8f1aab0
Jason Peak <jpeak5@lsu.edu>
2017-07-25 13:37:01 -0500

    proof of concept: adds http link to dc:description field

--- src/writers/LsuOaipmh.php
+++ src/writers/LsuOaipmh.php
@@ -1,32 +1,37 @@
 <?php
+
 namespace mik\writers;
 
 use GuzzleHttp\Client;
 use mik\exceptions\MikErrorException;
 use Monolog\Logger;
 
-class LsuOaipmh extends Oaipmh
-{
-    public function writeMetadataFile($metadata, $path, $overwrite = true)
-    {
-        // Add XML decleration
-        $doc = new \DomDocument('1.0');
-        $doc->loadXML($metadata);
-        $doc->formatOutput = true;
-        $root = $doc->getElementsByTagNameNS('http://www.openarchives.org/OAI/2.0/oai_dc/','dc')->item(0);
-        $elem = $doc->getElementsByTagNameNS('http://purl.org/dc/elements/1.1/','identifier');
-        $pid_elem = $elem->item(0)->textContent;
-        $pid_path = 'http://digitallibrary.tulane.edu/islandora/object/'.$pid_elem;
-        $link_back = $doc->createElementNS('http://purl.org/dc/elements/1.1/','dc:identifier',$pid_path);
-        $root->appendChild($link_back);
-        $metadata = $doc->saveXML();
+class LsuOaipmh extends Oaipmh {
 
-        if ($path !='') {
-            $fileCreationStatus = file_put_contents($path, $metadata);
-            if ($fileCreationStatus === false) {
-                $this->log->addWarning("There was a problem writing the metadata to a file",
-                    array('file' => $path));
-            }
-        }
+  public function writeMetadataFile($metadata, $path, $overwrite = true) {
+    // Add XML decleration
+    $doc = new \DomDocument('1.0');
+    $doc->loadXML($metadata);
+    $doc->formatOutput = true;
+
+    $pid = $doc->getElementsByTagNameNS('http://purl.org/dc/elements/1.1/', 'identifier')->item(0)->textContent;
+    $pid_path = htmlspecialchars("http://digitallibrary.tulane.edu/islandora/object/$pid");
+
+    $descEl = $doc->getElementsByTagNameNS('http://purl.org/dc/elements/1.1/', 'description')->item(0);
+    $description = sprintf("See full record: %s  %s", $descEl->textContent, $pid_path);
+
+    $newDescEl = $doc->createElementNS('http://purl.org/dc/elements/1.1/', 'dc:description', $description);
+
+    $root = $doc->getElementsByTagNameNS('http://www.openarchives.org/OAI/2.0/oai_dc/', 'dc')->item(0);
+    $root->replaceChild($newDescEl, $descEl);
+    $metadata = $doc->saveXML();
+
+    if ($path != '') {
+      $fileCreationStatus = file_put_contents($path, $metadata);
+      if ($fileCreationStatus === false) {
+        $this->log->addWarning("There was a problem writing the metadata to a file", array('file' => $path));
+      }
     }
+  }
+
 }
